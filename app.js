5d6a4f09e9753be77574528873b559e0a9747b08
DiegoElgart <diegojelgart@gmail.com>
2022-06-30 14:46:53 +0300

    added review validation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const { campgroundSchema } = require("./schemas");
+const { campgroundSchema, reviewSchema } = require("./schemas");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const metodOverride = require("method-override");
@@ -28,6 +28,16 @@ app.use(metodOverride("_method"));
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(",");
         throw new ExpressError(msg, 400);
@@ -104,12 +114,14 @@ app.delete(
 
 app.post(
     "/campgrounds/:id/reviews",
+    validateReview,
     catchAsync(async (req, res) => {
         const campground = await Campground.findById(req.params.id);
         const review = new Review(req.body.review);
         campground.reviews.push(review);
         await review.save();
         await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
     })
 );
 
